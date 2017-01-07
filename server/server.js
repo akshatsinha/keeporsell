418e82ef12f4c3f295c851e00bf95c1b9ccda57c
Akshat Sinha <akshatsinha@hotmail.com>
2017-01-06 22:32:04 -0800

    Added hot reload

--- server/server.js
+++ server/server.js
@@ -7,13 +7,21 @@ import path from 'path'
 
 import webpack from 'webpack'
 import webpackMiddleware from 'webpack-dev-middleware'
+import webpackHotMiddleware from 'webpack-hot-middleware'
 import webpackConfig from '../webpack.config.dev'
 
 import dashboardRoutes from './routes/dashboard'
 
 let app = express()
 
-app.use(webpackMiddleware(webpack(webpackConfig)))
+const compiler = webpack(webpackConfig)
+
+app.use(webpackMiddleware(compiler, {
+    hot: true,
+    publicPath: webpackConfig.output.publicPath,
+    noInfo: true // eliminate noise from webpack
+}))
+app.use(webpackHotMiddleware(compiler))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './index.html'))
