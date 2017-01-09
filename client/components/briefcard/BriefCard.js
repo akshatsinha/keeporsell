3b974a9abd90cc6b2f78d92314ee0b7d9a929f3a
Akshat Sinha <akshatsinha@hotmail.com>
2017-01-09 10:45:51 -0800

    styles for bc

--- client/components/briefcard/BriefCard.js
+++ client/components/briefcard/BriefCard.js
@@ -26,9 +26,14 @@ class BriefCard extends React.Component {
         return this.state.allSymbolsBrief.map((info) => {
             return (
                 <li key={info.id} className="bc-container">
-                    <h2>{info.symbol}</h2>
-                    <h4>{info.name}</h4>
-                    <p>{info.sector}</p>
+                    <div className="bc-left-text">
+                        <h2>{info.symbol}</h2>
+                        <h4>{info.name}</h4>
+                        <p>{info.sector}</p>
+                    </div>
+                    <div className="bc-center-text">
+                        <h3>{info.high_date.rt}</h3>
+                    </div>
                 </li>
             )
         })
